9e056a18453e238212ba86c81f002282a807f430
kimseaweed <128600341+kimseaweed@users.noreply.github.com>
2023-07-27 16:49:21 +0900

    first commit
    프로젝트 시작

---
+++ gamto/src/test/java/com/green/gamto/GamtoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.green.gamto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamtoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
