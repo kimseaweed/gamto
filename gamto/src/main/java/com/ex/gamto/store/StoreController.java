e63a1c1ea8ca44fbf05f81ed0faaf2e49f2a5855
samuelmincheol <140695950+samuelmincheol@users.noreply.github.com>
2023-08-01 12:18:46 +0900

    mvcpakage
    create mvcpakage and jspfolder

--- gamto/src/main/java/com/ex/gamto/store/StoreController.java
+++ gamto/src/main/java/com/ex/gamto/store/StoreController.java
@@ -1,5 +1,10 @@
 package com.ex.gamto.store;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/store")
 public class StoreController {
 
 }
