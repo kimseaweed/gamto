6ad02fe4ccfcbd0668bb7ba99a6d2438b3c4c4bb
samuelmincheol <140695950+samuelmincheol@users.noreply.github.com>
2023-08-14 11:33:06 +0900

    kakaoPay

---
+++ gamto/src/main/java/com/mrmr/gamto/kakaoPay/KakaoPayController.java
@@ -0,0 +1,47 @@
+package com.mrmr.gamto.kakaoPay;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.mrmr.gamto.kakaoPay.service.KakaoPay;
+
+import lombok.Setter;
+
+@Controller
+public class KakaoPayController {
+
+	
+	private static final Logger log = LoggerFactory.getLogger(KakaoPayController.class);
+
+	//@Setter(onMethod_ = @Autowired)
+	@Autowired
+    private KakaoPay kakaopay;
+    
+    
+    @GetMapping("/kakaoPay")
+    public void kakaoPayGet() {
+        
+    }
+    
+    @PostMapping("/kakaoPay")
+    public String kakaoPay() {
+        log.info("kakaoPay post............................................");
+        
+        return "redirect:" + kakaopay.kakaoPayReady();
+ 
+    }
+    
+    @GetMapping("/kakaoPaySuccess")
+    public void kakaoPaySuccess(@RequestParam("pg_token") String pg_token, Model model) {
+        log.info("kakaoPaySuccess get............................................");
+        log.info("kakaoPaySuccess pg_token : " + pg_token);
+        
+        model.addAttribute("info", kakaopay.kakaoPayInfo(pg_token));
+    }
+}
